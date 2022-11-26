01262ca211d857fb37f2ac93be8bdf0ebe626bf5
Artyom_Khachaturyan <artchief@gmail.com>
2022-11-26 19:39:55 +0300

    a

---
+++ z1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Write number1: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Write number2: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+// возводим в степень(число, степень)
+int Power(int num1, int num2)
+{
+    int result = 1;
+
+    for (int i = 0; i < num2; i++)
+    {
+        result *= num1;
+    }
+    return result;
+}
+// решение до сюда
+
+double res = Power (A, B);
+Console.WriteLine(res);
---
+++ z2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Write number");
+int num = Convert.ToInt32(Console.ReadLine());
+// метод с суммой чисел
+Console.WriteLine("Сумма " + GetSumNum(num));
+
+int GetSumNum(int input)
+{
+    // сумма чисел
+    int sum =0;
+    while(input>0)
+    {
+        sum+=input%10;
+        input=input/10;
+    }
+    return sum;
+}
